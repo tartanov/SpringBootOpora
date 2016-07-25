0131f95fa39ebb0125e0aa7ffeb014ef53dd2c82
tartanov.mikhail <tartanov.mikhail@otr.ru>
2016-07-25 18:56:19 +0300

    initial commit smevclient project

---
+++ smev3client/src/test/java/ru/otr/integration/smev3client/Smev3clientApplicationTests.java
@@ -0,0 +1,18 @@
+package ru.otr.integration.smev3client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Smev3clientApplication.class)
+@WebAppConfiguration
+public class Smev3clientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ smev3client/src/test/java/ru/otr/opora/integration/smev3client/Smev3clientApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.otr.opora.integration.smev3client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Smev3clientApplication.class)
+public class Smev3clientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
